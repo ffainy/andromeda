88a130043c2f885e46b2408418db594eae8fc07c
spiegelspike <spiegelspike@duck.com>
2024-08-22 14:29:09 +0800

    fix(quest): fix wowhead link

--- src/modules/quest/wowhead-link.lua
+++ src/modules/quest/wowhead-link.lua
@@ -4,66 +4,70 @@ local QUEST = F:GetModule('Quest')
 local linkQuest = 'http://www.wowhead.com/quest=%d'
 local linkAchievement = 'http://www.wowhead.com/achievement=%d'
 
-local function Button_Onclick(self)
-    if self.id and IsControlKeyDown() then
-        local text = linkAchievement:format(self.id)
-        _G.StaticPopup_Show('ANDROMEDA_WOWHEAD_LINK', _, _, text)
+local ID
+local headers = {
+    BonusObjectiveTracker,
+    CampaignQuestObjectiveTracker,
+    QuestObjectiveTracker,
+    AchievementObjectiveTracker,
+    WorldQuestObjectiveTracker,
+}
+
+-- Right click on quest or achievement on the ObjectiveTracker to get the wowhead link
+
+local function insertMenu()
+    for i = 1, #headers do
+        local tracker = headers[i]
+        if tracker then
+            hooksecurefunc(tracker, 'OnBlockHeaderClick', function(_, block)
+                ID = block.id
+            end)
+        end
     end
+
+    Menu.ModifyMenu('MENU_QUEST_OBJECTIVE_TRACKER', function(_, rootDescription)
+        rootDescription:CreateButton(L['Wowhead Link'], function()
+            local text = linkQuest:format(ID)
+            StaticPopup_Show('ANDROMEDA_WOWHEAD_LINK', _, _, text)
+        end)
+    end)
+
+    Menu.ModifyMenu('MENU_BONUS_OBJECTIVE_TRACKER', function(_, rootDescription)
+        rootDescription:CreateButton(L['Wowhead Link'], function()
+            local text = linkQuest:format(ID)
+            StaticPopup_Show('ANDROMEDA_WOWHEAD_LINK', _, _, text)
+        end)
+    end)
+
+    Menu.ModifyMenu('MENU_ACHIEVEMENT_TRACKER', function(_, rootDescription)
+        rootDescription:CreateButton(L['Wowhead Link'], function()
+            local text = linkAchievement:format(ID)
+            StaticPopup_Show('ANDROMEDA_WOWHEAD_LINK', _, _, text)
+        end)
+    end)
 end
 
-function QUEST:Load(addon)
-    if addon == 'Blizzard_AchievementUI' then
-        hooksecurefunc('AchievementButton_OnClick', Button_Onclick)
+-- Hold ctrl and click on the achievement in the AchievementUI to get the wowhead link
 
-        F:UnregisterEvent(self, QUEST.Load)
+local function hook(event, addon)
+    if addon == 'Blizzard_AchievementUI' then
+        hooksecurefunc(AchievementTemplateMixin, 'OnClick', function(self)
+            local elementData = self:GetElementData()
+            if elementData and elementData.id and IsControlKeyDown() then
+                local text = linkAchievement:format(elementData.id)
+                StaticPopup_Show('ANDROMEDA_WOWHEAD_LINK', nil, nil, text)
+            end
+        end)
+
+        F:UnregisterEvent(event, hook)
     end
 end
 
 function QUEST:WowheadLink()
-    if C.IS_WW then return end --#FIXME
     if not C.DB.Quest.WowheadLink then
         return
     end
 
-    hooksecurefunc('QuestObjectiveTracker_OnOpenDropDown', function(self)
-        local id = self.activeFrame.id
-        local info = _G.UIDropDownMenu_CreateInfo()
-        info.text = L['Wowhead Link']
-        info.func = function()
-            local text = linkQuest:format(id)
-            _G.StaticPopup_Show('ANDROMEDA_WOWHEAD_LINK', _, _, text)
-        end
-        info.notCheckable = true
-        _G.UIDropDownMenu_AddButton(info, _G.UIDROPDOWN_MENU_LEVEL)
-    end)
-
-    hooksecurefunc('BonusObjectiveTracker_OnOpenDropDown', function(self)
-        local id = self.activeFrame.TrackedQuest.questID
-        local info = _G.UIDropDownMenu_CreateInfo()
-        info.text = L['Wowhead Link']
-        info.func = function()
-            local text = linkQuest:format(id)
-            _G.StaticPopup_Show('ANDROMEDA_WOWHEAD_LINK', _, _, text)
-        end
-        info.notCheckable = true
-        _G.UIDropDownMenu_AddButton(info, _G.UIDROPDOWN_MENU_LEVEL)
-    end)
-
-    hooksecurefunc('AchievementObjectiveTracker_OnOpenDropDown', function(self)
-        local id = self.activeFrame.id
-        local info = _G.UIDropDownMenu_CreateInfo()
-        info.text = L['Wowhead Link']
-        info.func = function()
-            local text = linkAchievement:format(id)
-            _G.StaticPopup_Show('ANDROMEDA_WOWHEAD_LINK', _, _, text)
-        end
-        info.notCheckable = true
-        _G.UIDropDownMenu_AddButton(info, _G.UIDROPDOWN_MENU_LEVEL)
-    end)
-
-    -- if C_AddOns.IsAddOnLoaded('Blizzard_AchievementUI') then
-    --     hooksecurefunc('AchievementButton_OnClick', Button_Onclick)
-    -- else
-    --     F:RegisterEvent('ADDON_LOADED', QUEST.Load)
-    -- end
+    insertMenu()
+    F:RegisterEvent('ADDON_LOADED', hook)
 end
