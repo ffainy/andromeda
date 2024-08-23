2f5dda9347729ffcf50fffd4d3352f242dd6b608
spiegelspike <spiegelspike@duck.com>
2024-08-23 21:32:57 +0800

    other: cleanup

--- src/gui/gui.lua
+++ src/gui/gui.lua
@@ -636,6 +636,23 @@ do
     -- PlaySound(850) --IG_MAINMENU_OPEN
     -- PlaySound(854) --IG_MAINMENU_QUIT
 
+    GameMenuFrame.Header:SetWidth(139.3777)
+    GameMenuFrame.Header.Text:SetFontObject('GameFontNormal')
+
+    GameMenuFrame:HookScript('OnShow', function(self)
+        self.topPadding = 32
+        self.leftPadding = 26
+        self.rightPadding = 26
+        self.bottomPadding = 18
+
+        for button in GameMenuFrame.buttonPool:EnumerateActive() do
+            button:SetSize(144, 21)
+            button:SetNormalFontObject('GameFontHighlight')
+            button:SetHighlightFontObject('GameFontHighlight')
+            button:SetDisabledFontObject('GameFontDisable')
+        end
+    end)
+
     local StoreEnabled = C_StorePublic.IsEnabled
     local gameMenuLastButtons = {
         [GAMEMENU_OPTIONS] = 1,
