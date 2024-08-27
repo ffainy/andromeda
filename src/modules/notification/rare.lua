b70dce7337b2dc13192a041fb152dac7ec9730b8
spiegelspike <spiegelspike@duck.com>
2024-08-27 13:30:50 +0800

    other: remove debug stuff

--- src/modules/notification/rare.lua
+++ src/modules/notification/rare.lua
@@ -50,8 +50,7 @@ local function onEvent(_, id)
             local x, y = position:GetXY()
             nameStr = format(rareStr, mapID, x * 10000, y * 10000, info.name, x * 100, y * 100, '')
         end
-        local debugStr = C.IS_DEVELOPER and info.vignetteID or ''
-        F:Print(tex .. C.INFO_COLOR .. (nameStr or info.name or '') .. debugStr)
+        F:Print(tex .. C.INFO_COLOR .. (nameStr or info.name or ''))
 
         cache[id] = true
     end
