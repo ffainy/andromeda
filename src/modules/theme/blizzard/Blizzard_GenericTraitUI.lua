d401a39a21135dbbe15005605b47b3a19318da9e
spiegelspike <spiegelspike@duck.com>
2024-08-30 18:46:02 +0800

    improvement(theme): cleanup GenericTraitUI

--- src/modules/theme/blizzard/Blizzard_GenericTraitUI.lua
+++ src/modules/theme/blizzard/Blizzard_GenericTraitUI.lua
@@ -1,7 +1,10 @@
 local F, C = unpack(select(2, ...))
 
 C.Themes['Blizzard_GenericTraitUI'] = function()
-    local frame = _G.GenericTraitFrame
+    local frame = GenericTraitFrame
+
+    frame.BorderOverlay:SetAlpha(0)
+    frame.Background:SetAlpha(0)
 
     F.StripTextures(frame)
     F.ReskinClose(frame.CloseButton)
