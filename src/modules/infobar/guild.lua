ddd01d2604d65eac7ff9f98626476b3847ecb77c
spiegelspike <spiegelspike@duck.com>
2024-09-02 22:49:31 +0800

    fix(infobar): fix guild block

--- src/modules/infobar/guild.lua
+++ src/modules/infobar/guild.lua
@@ -1,24 +1,24 @@
 local F, C, L = unpack(select(2, ...))
 local INFOBAR = F:GetModule('InfoBar')
 
-INFOBAR.GuildTable = {}
-
+local newBlock
 local infoFrame, gName, gOnline, gRank, prevTime
+local guildTable = {}
 
 local function rosterButtonOnClick(self, btn)
-    local name = INFOBAR.GuildTable[self.index][3]
+    local name = guildTable[self.index][3]
     if btn == 'LeftButton' then
         if IsAltKeyDown() then
             C_PartyInfo.InviteUnit(name)
         elseif IsShiftKeyDown() then
-            if _G.MailFrame:IsShown() then
-                _G.MailFrameTab_OnClick(nil, 2)
-                _G.SendMailNameEditBox:SetText(name)
-                _G.SendMailNameEditBox:HighlightText()
+            if MailFrame:IsShown() then
+                MailFrameTab_OnClick(nil, 2)
+                SendMailNameEditBox:SetText(name)
+                SendMailNameEditBox:HighlightText()
             else
-                local editBox = _G.ChatEdit_ChooseBoxForSend()
+                local editBox = ChatEdit_ChooseBoxForSend()
                 local hasText = (editBox:GetText() ~= '')
-                _G.ChatEdit_ActivateChat(editBox)
+                ChatEdit_ActivateChat(editBox)
                 editBox:Insert(name)
                 if not hasText then
                     editBox:HighlightText()
@@ -26,53 +26,48 @@ local function rosterButtonOnClick(self, btn)
             end
         end
     else
-        _G.ChatFrame_OpenChat('/w ' .. name .. ' ', _G.SELECTED_DOCK_FRAME)
+        ChatFrame_OpenChat('/w ' .. name .. ' ', SELECTED_DOCK_FRAME)
     end
 end
 
 function INFOBAR:GuildPanel_CreateButton(parent, index)
     local button = CreateFrame('Button', nil, parent)
-    button:SetSize(305, 20)
+    button:SetSize(370, 20)
     button:SetPoint('TOPLEFT', 0, -(index - 1) * 20)
     button.HL = button:CreateTexture(nil, 'HIGHLIGHT')
     button.HL:SetAllPoints()
     button.HL:SetColorTexture(C.r, C.g, C.b, 0.2)
 
-    local outline = _G.ANDROMEDA_ADB.FontOutline
-    button.level =
-        F.CreateFS(button, C.Assets.Fonts.Regular, 13, outline or nil, 'Level', nil, outline and 'NONE' or 'THICK')
+    local outline = ANDROMEDA_ADB.FontOutline
+
+    button.level = F.CreateFS(
+        button,
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        'Level', nil, outline and 'NONE' or 'THICK'
+    )
     button.level:SetPoint('TOP', button, 'TOPLEFT', 16, -4)
+
     button.class = button:CreateTexture(nil, 'ARTWORK')
     button.class:SetPoint('LEFT', 40, 0)
     button.class:SetSize(16, 16)
     button.class:SetTexture('Interface\\GLUES\\CHARACTERCREATE\\UI-CHARACTERCREATE-CLASSES')
+
     button.name = F.CreateFS(
         button,
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        'Name',
-        nil,
-        outline and 'NONE' or 'THICK',
-        'LEFT',
-        70,
-        0
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        'Name', nil, outline and 'NONE' or 'THICK',
+        { 'LEFT', 70, 0 }
     )
-    button.name:SetPoint('RIGHT', button, 'LEFT', 185, 0)
+    button.name:SetPoint('RIGHT', button, 'LEFT', 250, 0)
     button.name:SetJustifyH('LEFT')
+
     button.zone = F.CreateFS(
         button,
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        'Zone',
-        nil,
-        outline and 'NONE' or 'THICK',
-        'RIGHT',
-        -2,
-        0
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        'Zone', nil, outline and 'NONE' or 'THICK',
+        {'RIGHT', -2, 0}
     )
-    button.zone:SetPoint('LEFT', button, 'RIGHT', -120, 0)
+    button.zone:SetPoint('LEFT', button.name, 'RIGHT', -10, 0)
     button.zone:SetJustifyH('RIGHT')
     button.zone:SetWordWrap(false)
 
@@ -84,7 +79,7 @@ end
 
 function INFOBAR:GuildPanel_UpdateButton(button)
     local index = button.index
-    local level, class, name, zone, status, guid = unpack(INFOBAR.guildTable[index])
+    local level, class, name, zone, status, guid = unpack(guildTable[index])
 
     local levelcolor = F:RgbToHex(GetQuestDifficultyColor(level))
     button.level:SetText(levelcolor .. level)
@@ -106,12 +101,12 @@ function INFOBAR:GuildPanel_UpdateButton(button)
 end
 
 function INFOBAR:GuildPanel_Update()
-    local scrollFrame = _G[C.ADDON_TITLE .. 'GuildInfobarScrollFrame']
+    local scrollFrame = _G[C.ADDON_TITLE .. 'InfobarGuildScrollFrame']
     local usedHeight = 0
     local buttons = scrollFrame.buttons
     local height = scrollFrame.buttonHeight
     local numMemberButtons = infoFrame.numMembers
-    local offset = _G.HybridScrollFrame_GetOffset(scrollFrame)
+    local offset = HybridScrollFrame_GetOffset(scrollFrame)
 
     for i = 1, #buttons do
         local button = buttons[i]
@@ -127,7 +122,7 @@ function INFOBAR:GuildPanel_Update()
         end
     end
 
-    _G.HybridScrollFrame_Update(scrollFrame, numMemberButtons * height, usedHeight)
+    HybridScrollFrame_Update(scrollFrame, numMemberButtons * height, usedHeight)
 end
 
 function INFOBAR:GuildPanel_OnMouseWheel(delta)
@@ -142,22 +137,22 @@ end
 
 local function sortRosters(a, b)
     if a and b then
-        if _G.ANDROMEDA_ADB['GuildSortOrder'] then
-            return a[_G.ANDROMEDA_ADB['GuildSortBy']] < b[_G.ANDROMEDA_ADB['GuildSortBy']]
+        if ANDROMEDA_ADB['GuildSortOrder'] then
+            return a[ANDROMEDA_ADB['GuildSortBy']] < b[ANDROMEDA_ADB['GuildSortBy']]
         else
-            return a[_G.ANDROMEDA_ADB['GuildSortBy']] > b[_G.ANDROMEDA_ADB['GuildSortBy']]
+            return a[ANDROMEDA_ADB['GuildSortBy']] > b[ANDROMEDA_ADB['GuildSortBy']]
         end
     end
 end
 
 function INFOBAR:GuildPanel_SortUpdate()
-    sort(INFOBAR.GuildTable, sortRosters)
+    sort(guildTable, sortRosters)
     INFOBAR:GuildPanel_Update()
 end
 
 local function sortHeaderOnClick(self)
-    _G.ANDROMEDA_ADB['GuildSortBy'] = self.index
-    _G.ANDROMEDA_ADB['GuildSortOrder'] = not _G.ANDROMEDA_ADB['GuildSortOrder']
+    ANDROMEDA_ADB['GuildSortBy'] = self.index
+    ANDROMEDA_ADB['GuildSortOrder'] = not ANDROMEDA_ADB['GuildSortOrder']
     INFOBAR:GuildPanel_SortUpdate()
 end
 
@@ -181,8 +176,8 @@ function INFOBAR:GuildPanel_Init()
 
     local anchorTop = C.DB.Infobar.AnchorTop
 
-    infoFrame = CreateFrame('Frame', C.ADDON_TITLE .. 'GuildInfobar', INFOBAR.Bar)
-    infoFrame:SetSize(335, 495)
+    infoFrame = CreateFrame('Frame', C.ADDON_TITLE .. 'InfoBarGuildFrame', INFOBAR.Bar)
+    infoFrame:SetSize(400, 495) -- 335
     infoFrame:SetPoint(
         anchorTop and 'TOP' or 'BOTTOM',
         INFOBAR.GuildBlock,
@@ -198,47 +193,28 @@ function INFOBAR:GuildPanel_Init()
         self:SetScript('OnUpdate', isPanelCanHide)
     end)
 
-    local outline = _G.ANDROMEDA_ADB.FontOutline
+    local outline = ANDROMEDA_ADB.FontOutline
     gName = F.CreateFS(
         infoFrame,
-        C.Assets.Fonts.Bold,
-        16,
-        outline or nil,
-        'Guild',
-        nil,
-        outline and 'NONE' or 'THICK',
-        'TOPLEFT',
-        15,
-        -10
+        C.Assets.Fonts.Bold, 16, outline or nil,
+        'Guild', nil, outline and 'NONE' or 'THICK',
+        { 'TOPLEFT', 15, -10 }
     )
     gOnline = F.CreateFS(
         infoFrame,
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        'Online',
-        nil,
-        outline and 'NONE' or 'THICK',
-        'TOPLEFT',
-        15,
-        -35
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        'Online', nil, outline and 'NONE' or 'THICK',
+        { 'TOPLEFT', 15, -35 }
     )
-    -- gApps = F.CreateFS(infoFrame, C.Assets.Fonts.Regular, 13, outline or nil, 'Applications', nil, outline and 'NONE' or 'THICK', 'TOPRIGHT', -15, -35)
     gRank = F.CreateFS(
         infoFrame,
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        'Rank',
-        nil,
-        outline and 'NONE' or 'THICK',
-        'TOPLEFT',
-        15,
-        -51
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        'Rank', nil, outline and 'NONE' or 'THICK',
+        { 'TOPLEFT', 15, -51 }
     )
 
     local bu = {}
-    local width = { 30, 35, 126, 126 }
+    local width = { 30, 35, 191, 126 }
     for i = 1, 4 do
         bu[i] = CreateFrame('Button', nil, infoFrame)
         bu[i]:SetSize(width[i], 22)
@@ -254,88 +230,61 @@ function INFOBAR:GuildPanel_Init()
         bu[i].index = i
         bu[i]:SetScript('OnClick', sortHeaderOnClick)
     end
-    F.CreateFS(bu[1], C.Assets.Fonts.Regular, 13, outline or nil, _G.LEVEL, nil, outline and 'NONE' or 'THICK')
-    F.CreateFS(bu[2], C.Assets.Fonts.Regular, 13, outline or nil, _G.CLASS, nil, outline and 'NONE' or 'THICK')
+    F.CreateFS(
+        bu[1],
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        LEVEL, nil, outline and 'NONE' or 'THICK'
+    )
+    F.CreateFS(
+        bu[2],
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        CLASS, nil, outline and 'NONE' or 'THICK'
+    )
     F.CreateFS(
         bu[3],
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        _G.NAME,
-        nil,
-        outline and 'NONE' or 'THICK',
-        'LEFT',
-        5,
-        0
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        NAME,
+        nil, outline and 'NONE' or 'THICK',
+        { 'LEFT', 5, 0 }
     )
     F.CreateFS(
         bu[4],
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        _G.ZONE,
-        nil,
-        outline and 'NONE' or 'THICK',
-        'RIGHT',
-        -5,
-        0
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        ZONE, nil, outline and 'NONE' or 'THICK',
+        { 'RIGHT', -5, 0 }
     )
 
     F.CreateFS(
         infoFrame,
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        C.LINE_STRING,
-        nil,
-        outline and 'NONE' or 'THICK',
-        'BOTTOMRIGHT',
-        -12,
-        58
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        C.LINE_STRING, nil, outline and 'NONE' or 'THICK',
+        { 'BOTTOMRIGHT', -12, 58 }
     )
-    local whspInfo = C.INFO_COLOR .. C.MOUSE_RIGHT_BUTTON .. L['Whisper']
+    local whspInfo = C.MOUSE_RIGHT_BUTTON .. L['Whisper']
     F.CreateFS(
         infoFrame,
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        whspInfo,
-        nil,
-        outline and 'NONE' or 'THICK',
-        'BOTTOMRIGHT',
-        -15,
-        42
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        whspInfo, {0.9, 0.8, 0.6}, outline and 'NONE' or 'THICK',
+        { 'BOTTOMRIGHT', -15, 42 }
     )
-    local invtInfo = C.INFO_COLOR .. 'ALT +' .. C.MOUSE_LEFT_BUTTON .. L['Invite']
+    local invtInfo = 'ALT +' .. C.MOUSE_LEFT_BUTTON .. L['Invite']
     F.CreateFS(
         infoFrame,
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        invtInfo,
-        nil,
-        outline and 'NONE' or 'THICK',
-        'BOTTOMRIGHT',
-        -15,
-        26
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        invtInfo, {0.9, 0.8, 0.6}, outline and 'NONE' or 'THICK',
+        { 'BOTTOMRIGHT', -15, 26 }
     )
-    local copyInfo = C.INFO_COLOR .. 'SHIFT +' .. C.MOUSE_LEFT_BUTTON .. L['Copy Name']
+    local copyInfo = 'SHIFT +' .. C.MOUSE_LEFT_BUTTON .. L['Copy Name']
     F.CreateFS(
         infoFrame,
-        C.Assets.Fonts.Regular,
-        13,
-        outline or nil,
-        copyInfo,
-        nil,
-        outline and 'NONE' or 'THICK',
-        'BOTTOMRIGHT',
-        -15,
-        10
+        C.Assets.Fonts.Regular, 13, outline or nil,
+        copyInfo, {0.9, 0.8, 0.6}, outline and 'NONE' or 'THICK',
+        { 'BOTTOMRIGHT', -15, 10 }
     )
 
-    local scrollFrame =
-        CreateFrame('ScrollFrame', C.ADDON_TITLE .. 'GuildInfobarScrollFrame', infoFrame, 'HybridScrollFrameTemplate')
-    scrollFrame:SetSize(305, 320)
+    local scrollFrame = CreateFrame('ScrollFrame', C.ADDON_TITLE .. 'InfobarGuildScrollFrame', infoFrame,
+        'HybridScrollFrameTemplate')
+    scrollFrame:SetSize(370, 320)
     scrollFrame:SetPoint('TOPLEFT', 10, -100)
     infoFrame.scrollFrame = scrollFrame
 
@@ -376,53 +325,54 @@ function INFOBAR:GuildPanel_Refresh()
         prevTime = thisTime
     end
 
-    wipe(INFOBAR.GuildTable)
+    wipe(guildTable)
     local count = 0
     local total, _, online = GetNumGuildMembers()
     local guildName, guildRank = GetGuildInfo('player')
 
     gName:SetText(F:RgbToHex({ 0.9, 0.8, 0.6 }) .. '<' .. (guildName or '') .. '>')
-    gOnline:SetText(format(C.INFO_COLOR .. '%s:' .. ' %d/%d', _G.GUILD_ONLINE_LABEL, online, total))
-    -- gApps:SetText(format(C.INFO_COLOR .. _G.GUILDINFOTAB_APPLICANTS, GetNumGuildApplicants()))
-    gRank:SetText(C.INFO_COLOR .. _G.RANK .. ': ' .. (guildRank or ''))
+    gOnline:SetText(format(C.INFO_COLOR .. '%s:' .. ' %d/%d', GUILD_ONLINE_LABEL, online, total))
+    gRank:SetText(C.INFO_COLOR .. RANK .. ': ' .. (guildRank or ''))
 
     for i = 1, total do
-        local name, _, _, level, _, zone, _, _, connected, status, class, _, _, mobile, _, _, guid =
-            GetGuildRosterInfo(i)
+        local name, _, _, level, _, zone, _, _, connected, status, class, _, _, mobile, _, _, guid = GetGuildRosterInfo(
+        i)
         if connected or mobile then
             if mobile and not connected then
-                zone = _G.REMOTE_CHAT
+                zone = REMOTE_CHAT
                 if status == 1 then
                     status = '|TInterface\\ChatFrame\\UI-ChatIcon-ArmoryChat-AwayMobile:14:14:0:0:16:16:0:16:0:16|t'
                 elseif status == 2 then
                     status = '|TInterface\\ChatFrame\\UI-ChatIcon-ArmoryChat-BusyMobile:14:14:0:0:16:16:0:16:0:16|t'
                 else
-                    status = _G.ChatFrame_GetMobileEmbeddedTexture(73 / 255, 177 / 255, 73 / 255)
+                    status = ChatFrame_GetMobileEmbeddedTexture(73 / 255, 177 / 255, 73 / 255)
                 end
             else
                 if status == 1 then
-                    status = '|T' .. _G.FRIENDS_TEXTURE_AFK .. ':14:14:0:0:16:16:1:15:1:15|t'
+                    status = '|T' .. FRIENDS_TEXTURE_AFK .. ':14:14:0:0:16:16:1:15:1:15|t'
                 elseif status == 2 then
-                    status = '|T' .. _G.FRIENDS_TEXTURE_DND .. ':14:14:0:0:16:16:1:15:1:15|t'
+                    status = '|T' .. FRIENDS_TEXTURE_DND .. ':14:14:0:0:16:16:1:15:1:15|t'
                 else
                     status = ' '
                 end
             end
+
             if not zone then
-                zone = _G.UNKNOWN
+                zone = UNKNOWN
             end
 
             count = count + 1
 
-            if not INFOBAR.GuildTable[count] then
-                INFOBAR.GuildTable[count] = {}
+            if not guildTable[count] then
+                guildTable[count] = {}
             end
-            INFOBAR.GuildTable[count][1] = level
-            INFOBAR.GuildTable[count][2] = class
-            INFOBAR.GuildTable[count][3] = Ambiguate(name, 'none')
-            INFOBAR.GuildTable[count][4] = zone
-            INFOBAR.GuildTable[count][5] = status
-            INFOBAR.guildTable[count][6] = guid
+
+            guildTable[count][1] = level
+            guildTable[count][2] = class
+            guildTable[count][3] = Ambiguate(name, 'none')
+            guildTable[count][4] = zone
+            guildTable[count][5] = status
+            guildTable[count][6] = guid
         end
     end
 
@@ -443,18 +393,18 @@ local function Block_OnMouseUp(self)
 
     infoFrame:Hide()
 
-    if not _G.CommunitiesFrame then
+    if not CommunitiesFrame then
         C_AddOns.LoadAddOn('Blizzard_Communities')
     end
 
-    if _G.CommunitiesFrame then
-        ToggleFrame(_G.CommunitiesFrame)
+    if CommunitiesFrame then
+        ToggleFrame(CommunitiesFrame)
     end
 end
 
 local function Block_OnEvent(self, event, arg1)
     if not IsInGuild() then
-        self.text:SetText(_G.GUILD .. ': ' .. C.MY_CLASS_COLOR .. _G.NONE)
+        self.text:SetText(GUILD .. ': ' .. C.MY_CLASS_COLOR .. NONE)
         return
     end
 
@@ -465,7 +415,7 @@ local function Block_OnEvent(self, event, arg1)
     end
 
     local online = select(3, GetNumGuildMembers())
-    self.text:SetText(_G.GUILD .. ': ' .. C.MY_CLASS_COLOR .. online)
+    self.text:SetText(GUILD .. ': ' .. C.MY_CLASS_COLOR .. online)
 
     if infoFrame and infoFrame:IsShown() then
         INFOBAR:GuildPanel_Refresh()
@@ -477,8 +427,10 @@ local function Block_OnEnter(self)
     if not IsInGuild() then
         return
     end
-    if _G[C.ADDON_TITLE .. 'FriendsFrame'] and _G[C.ADDON_TITLE .. 'FriendsFrame']:IsShown() then
-        _G[C.ADDON_TITLE .. 'FriendsFrame']:Hide()
+
+    local friendsFrame = _G[C.ADDON_TITLE .. 'InfoBarFriendsFrame']
+    if friendsFrame and friendsFrame:IsShown() then
+        friendsFrame:Hide()
     end
 
     INFOBAR:GuildPanel_Init()
@@ -498,12 +450,16 @@ function INFOBAR:CreateGuildBlock()
         return
     end
 
-    local guild = INFOBAR:RegisterNewBlock('guild', 'RIGHT', 150)
-    guild.onEvent = Block_OnEvent
-    guild.onEnter = Block_OnEnter
-    guild.onLeave = Block_OnLeave
-    guild.onMouseUp = Block_OnMouseUp
-    guild.eventList = { 'PLAYER_ENTERING_WORLD', 'GUILD_ROSTER_UPDATE', 'PLAYER_GUILD_UPDATE' }
+    newBlock = INFOBAR:RegisterNewBlock('guild', 'RIGHT', 150)
+    newBlock.onEvent = Block_OnEvent
+    newBlock.onEnter = Block_OnEnter
+    newBlock.onLeave = Block_OnLeave
+    newBlock.onMouseUp = Block_OnMouseUp
+    newBlock.eventList = {
+        'PLAYER_ENTERING_WORLD',
+        'GUILD_ROSTER_UPDATE',
+        'PLAYER_GUILD_UPDATE',
+    }
 
-    INFOBAR.GuildBlock = guild
+    INFOBAR.GuildBlock = newBlock
 end
