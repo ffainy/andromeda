606da6c0017b64bdf8afb6e614e8f33d3bf2599f
spiegelspike <spiegelspike@duck.com>
2024-10-30 08:41:19 +0800

    improvement: update objective tracker

--- src/modules/theme/blizzard/FrameXML/ObjectiveTracker.lua
+++ src/modules/theme/blizzard/FrameXML/ObjectiveTracker.lua
@@ -189,7 +189,7 @@ tinsert(C.BlizzThemes, function()
     hooksecurefunc(ScenarioObjectiveTracker.StageBlock, 'UpdateStageBlock', function(block)
         block.NormalBG:SetTexture('')
         if not block.bg then
-            block.bg = F.SetBD(block.GlowTexture, nil, 4, -2, -4, 2)
+            block.bg = F.SetBD(block.GlowTexture, nil, 0, -2, 4, 2)
         end
     end)
 
