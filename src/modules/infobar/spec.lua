2eb845dfc08cecb9ab968ab08afc0cb16db53fbc
spiegelspike <spiegelspike@duck.com>
2025-03-02 20:45:23 +0800

    fix: replace 'SetSpecialization' with 'C_SpecializationInfo.SetSpecialization'

--- src/modules/infobar/spec.lua
+++ src/modules/infobar/spec.lua
@@ -14,7 +14,7 @@ local function selectSpec(_, specIndex)
     if currentSpecIndex == specIndex then
         return
     end
-    SetSpecialization(specIndex)
+    C_SpecializationInfo.SetSpecialization(specIndex)
     DropDownList1:Hide()
 end
 
